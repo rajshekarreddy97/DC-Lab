723e6b0dc112ede9ee52c10af7982d9c8988cbd0
Rajshekar Reddy <37366978+rajshekarreddy97@users.noreply.github.com>
2019-01-02 10:02:49 +0530

    commit

--- mpi_approx_pi.c
+++ mpi_approx_pi.c
@@ -2,7 +2,7 @@
 #include <mpi.h>
 #include <math.h>
 double dx_arctan(double x)
-{    
+{
     return (1.0 / (1.0 + x*x));
 }
 int main(int argc, char *argv[])
@@ -11,20 +11,20 @@ int main(int argc, char *argv[])
     double PI25DT = 3.141592653589793238462643;
     double mypi, h, pi, i, sum, x, a, startwtime, endwtime;
     int myid, numprocs, resultlen;
-    char name[MPI_MAX_PROCESSOR_NAME] ; 
+    char name[MPI_MAX_PROCESSOR_NAME] ;
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-    MPI_Get_processor_name(name, &resultlen); 
+    MPI_Get_processor_name(name, &resultlen);
     printf("This is Process-%d/%d running on %s \n",
            myid,numprocs,name);
     fflush(0);
     MPI_Barrier(MPI_COMM_WORLD);
-    if(myid == 0) 
+    if(myid == 0)
     {
         printf("This program uses %d processes\n", numprocs);
-	 n = 100000;
-        printf("The number of intervals = %d \n", n); 
+	      n = 100000;
+        printf("The number of intervals = %d \n", n);
         startwtime = MPI_Wtime();
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -40,8 +40,8 @@ int main(int argc, char *argv[])
     {
         endwtime = MPI_Wtime();
         printf("pi is approximately %.16f, Error is %.16f\n",
-                pi, fabs(pi - PI25DT)); 
-        printf("wall clock time = %f\n", endwtime-startwtime); 
+                pi, fabs(pi - PI25DT));
+        printf("wall clock time = %f\n", endwtime-startwtime);
     }
     MPI_Finalize();
     return 0;
